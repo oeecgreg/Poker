a9207c4e35a54e2d8c121dce5adfc54919641e2b
Fred Durst <alexanderbelhareth@gmail.com>
2026-03-16 10:37:18 +0100

    How to pronounce DIX?

---
+++ src/Carte.java
@@ -0,0 +1,9 @@
+public class Carte {
+    private Couleur couleur;
+    private Valeur valeur;
+
+    public Carte(Couleur couleur, Valeur valeur) {
+        this.couleur = couleur;
+        this.valeur = valeur;
+    }
+}
